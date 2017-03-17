173d9d6e8a7924bea4f31f7ab56a7a1f2c4ffd68
唐超 <tangchao@hbkn.cn>
2017-03-17 13:57:52 +0800

    f1类注释

--- 3DViewPagerGallery-master/app/src/main/java/com/example/evanzeng/viewpagertest/ReverseImageActivity.java
+++ 3DViewPagerGallery-master/app/src/main/java/com/example/evanzeng/viewpagertest/ReverseImageActivity.java
@@ -3,9 +3,13 @@ package com.example.evanzeng.viewpagertest;
 import android.support.v7.app.AppCompatActivity;
 
 /**
- * Created by Administrator on 2016/8/20.
+ * 类描述:
+ * 创建人:   tangchao
+ * 创建时间:  2017/3/17 13:57
+ * 修改人:    tangchao
+ * 修改时间: 2017/3/17 13:57
+ * 修改备注:  [说明本次修改内容]
  */
-
 public class ReverseImageActivity extends AppCompatActivity {
     //分支1
     //主线
